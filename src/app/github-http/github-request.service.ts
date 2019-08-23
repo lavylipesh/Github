a952d0168ab568e7ec72cb3aa783020d79e6d9f8
lavylipesh <lilibethlangat@yahoo.com>
2019-08-23 14:33:43 -0400

    add class github

--- src/app/github-http/github-request.service.ts
+++ src/app/github-http/github-request.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import{Github} from '../github'
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubRequestService {
+  github:Github[]=[];
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  serachUsers(serarchGithub:string){
+    let searchEndpoint=""
+  }
 }
