d578cf5c8c721073386a828ec5d26bf101312048
lavylipesh <lilibethlangat@yahoo.com>
2019-08-23 16:55:29 -0400

    click function

--- src/app/github-http/github-request.service.ts
+++ src/app/github-http/github-request.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from'@angular/common/http';
+import {environment} from '../../environments/environment';
+import {Github} from '../github';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubRequestService {
+  users:Github[]=[];
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  searchGithub(search:string){
+    interface Results{
+      data:[];
+    } 
+  
+    let searchEndpoint= "https://api.github.com/users/search?api_key=" + environment.GithubUrl;
+
+    let promise =  new Promise((resolve, reject)=>{
+        this.http.get<Results>(searchEndpoint+"&q="+search).toPromise().then(
+
+          (results)=>{
+            this.users=[];
+            for(let i=0; i<results["data"].length; i++){
+              this.users.push(new Github());
+              
+            }
+            console.log(this.users);
+            resolve()
+          },
+          (error)=>{
+            console.log(error)
+            reject()
+          }
+        )
+    })
+    return promise;
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
   GithubUrl:"b5e8cd8a999c688a5b3ff4b70f6b122c04e76263",
 };
 
